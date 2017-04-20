e1b645940dd34106cb231b0994c49a65118aa278
Pavel Onuchin <onuchinproduct@gmail.com>
2017-04-21 00:30:25 +0600

    completed

--- app/src/main/java/com/example/onuchinx/tablayout/MainActivity.java
+++ app/src/main/java/com/example/onuchinx/tablayout/MainActivity.java
@@ -1,13 +1,101 @@
 package com.example.onuchinx.tablayout;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Yosemite Park");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView image_header = (ImageView)findViewById(R.id.header);
+        image_header.setBackgroundResource(R.mipmap.yose);
+        setupViewPager();
+
+        setupCollapsingToolbar();
+
+        setupDrawer();
     }
+    private void setupDrawer() {
+
+
+    }
+
+    private void setupCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(
+                R.id.collapse_toolbar);
+
+        collapsingToolbar.setTitleEnabled(false);
+    }
+
+    private void setupViewPager() {
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new TabFragment(), "Information");
+        adapter.addFrag(new TabFragment(), "Programm");
+        adapter.addFrag(new TabFragment(), "Price");
+        adapter.addFrag(new TabFragment(),"Media");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    static class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
 }
